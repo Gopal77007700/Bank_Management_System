27875f19d229e8cc64373b106cf911e2b79c1e79
Gopal7700 <gopalpawar0077@gmail.com>
2025-09-24 02:02:48 +0900

    first commit

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,17 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setSize(850,480);
+        setLocation(450,200);
+        setVisible(true);
+
+    }
+
+    public static void main (String[] args){
+        new Login();
+
+    }
+}
